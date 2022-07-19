ac62d1cb40d23c09a089962b1f3c77f6e3670d24
ndr.singh <ndrsingh487@gmail.com>
2022-07-19 12:27:16 +0530

    new user registration app

---
+++ UserApp/UserApp/Models/VM/VMRegister.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserApp.Models.VM
+{
+    public class VMRegister
+    {
+        [Key]
+        public Guid UserId { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required (ErrorMessage ="Email is Required.")]
+        [RegularExpression(".+\\@.+\\..+")]
+        
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Phoneno { get; set; }
+    }
+}
